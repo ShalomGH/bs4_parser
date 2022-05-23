c19a02ffc90e80d2259c8c0f2ec3b4424c8dcb03
an1key <63707234+an1key@users.noreply.github.com>
2022-05-23 21:24:55 +0600

    some works in parser...

--- parser.py
+++ parser.py
@@ -6,7 +6,6 @@ def parser(url):
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    print(soup.prettify())
-
-
-
+    name = soup.find(class_="item-card-name-mobile").find('h1').text
+    properties = soup.find(class_="tabs-content").find(class_="tab")
+    print(properties)
