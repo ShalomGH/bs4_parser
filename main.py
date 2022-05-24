7175b37690b59045541a5dfd9d0bff5013ec340f
an1key <63707234+an1key@users.noreply.github.com>
2022-05-24 19:23:24 +0600

    working user choice between file or stdin

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ frame = pd.DataFrame(data)
 
 
 if __name__ == '__main__':
-    for link in get_links('links.txt'):
-        frame.loc[len(frame.index)] = parser(link)
-    print(frame)
+    print('Выберите:\n(1) - Выборка из файла "links.txt"\n(2) - Выборка из стандартного ввода')
+    FROM_FILE = input()
+    if FROM_FILE == 1:
+        for link in get_links('links.txt'):
+            frame.loc[len(frame.index)] = parser(link)
+        print(frame)
+    elif FROM_FILE == 2:
+        while True:
+            print('Введите ссылку на продукт')
+            try:
+                link = input()
+                frame.loc[len(frame.index)] = parser(link)
+                print(frame)
+            except:
+                print('Try again\n')
--- parser.py
+++ parser.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parser(url):
+def parser(url: str):
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    art = soup.find(class_="product_main-id").find(class_="").text.strip()
-    name = soup.find(class_="main-header").find("h1").text.strip()
+    art = soup.find(class_="product_main-id").find(class_="").text
+    name = soup.find(class_="main-header").find("h1").text
     cost = soup.find(class_="ordering__value").text.replace(u'\xa0', u' ')
     return [art, name, cost]
